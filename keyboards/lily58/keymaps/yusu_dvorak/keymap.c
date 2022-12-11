3bb783f1c42279e6ae1892a3e0c6ce193b7a0627
joopitz <46931073+San0ix@users.noreply.github.com>
2022-12-11 14:31:16 +0100

    change: gaming layer from QWERTY to DVORAK

--- keyboards/lily58/keymaps/yusu_dvorak/keymap.c
+++ keyboards/lily58/keymaps/yusu_dvorak/keymap.c
@@ -137,9 +137,37 @@ KC_TRNS, OSL(1), TO(0), KC_TRNS,
 KC_TRNS, TO(0), OSL(1), KC_TRNS
 ),
 
-[4] = LAYOUT(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_NO, KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_RSFT, KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, TO(0), TO(0), KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RCTL, KC_LGUI, KC_NO, LT(5,KC_TAB), KC_TRNS, KC_TRNS, LT(5,KC_ENT), KC_NO, KC_RGUI),
+[4] = LAYOUT(
+// first row
+// left hand
+KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,
+// right hand
+KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO,
 
-[5] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_7, KC_8, KC_9, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_4, KC_5, KC_6, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_1, KC_2, KC_3, KC_NO, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
+// second row
+// left hand
+KC_TAB, KC_SCLN, KC_COMM, KC_DOT, KC_P, KC_Y,
+// right hand
+KC_F, KC_G, KC_C, KC_R, KC_L, KC_GRV,
+
+// third row
+// left hand
+KC_BSLS, KC_A, KC_O, KC_E, KC_U, KC_I,
+// right hand
+KC_D, KC_H, KC_T, KC_N, KC_S, KC_ENT,
+
+// fourth row
+// left hand
+KC_TAB, KC_QUOT, KC_Q, KC_J, KC_K, KC_X,
+// thumb keys
+TO(0), TO(0),
+// right hand
+KC_B, KC_M, KC_W, KC_V, KC_Z, KC_NO,
+
+// thumb keys
+KC_LALT, KC_LCTL, KC_LSFT, KC_SPC,
+KC_BSPC, KC_RSFT, KC_LCTL, KC_ESC
+),
 
 };
 
